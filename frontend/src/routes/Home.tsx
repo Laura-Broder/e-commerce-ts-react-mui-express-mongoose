dc2054fbdd02592c6df85966d5d40bd4a9f0ad21
LauraB <67835295+Laura-Broder@users.noreply.github.com>
2023-05-05 13:35:46 +0300

    show products on homepage too

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,7 +1,19 @@
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import Products from "./Products";
+
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Typography variant="h4" component="div" p={8}>
+        home page promotions
+      </Typography>
+      <Divider variant="middle" />
+      <Products />
+    </div>
+  );
 };
 
 export default Home;
